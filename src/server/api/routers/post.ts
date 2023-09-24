3cf456bf14c92efec50f096d5c9d170ddf5f9744
Adam Szi <szi.adam@simonyi.bme.hu>
2023-09-24 17:48:47 +0200

    added posts with updated page styling

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,7 +1,48 @@
+import { type User, clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postRouter = createTRPCRouter({
-  list: publicProcedure.query(({ ctx }) => {
-    return ctx.db.post.findMany();
+  list: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.db.post.findMany({
+      take: 100,
+    });
+
+    const users = await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+    });
+
+    const clientUserData = users.map((user) => filterUserForClient(user));
+
+    return posts.map((post) => {
+      const author = clientUserData.find((user) => user.id === post.authorId);
+
+      if (!author) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: `Author for post (${post.id}) not found`,
+        });
+      }
+
+      return {
+        ...post,
+        author,
+      };
+    });
   }),
 });
+
+function filterUserForClient(user: User) {
+  if (!user.username) {
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: `No username for user (${user.id})`,
+    });
+  }
+
+  return {
+    id: user.id,
+    username: user.username,
+    imageUrl: user.imageUrl,
+  };
+}
