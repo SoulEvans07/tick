4d466aaeb52d0d47c16f8dd82ef1ed2a02126b44
Adam Szi <szi.adam@simonyi.bme.hu>
2023-09-24 22:55:44 +0200

    update env check and example

--- src/env.mjs
+++ src/env.mjs
@@ -18,6 +18,7 @@ export const env = createEnv({
       .enum(['development', 'test', 'production'])
       .default('development'),
     CLERK_SECRET_KEY: z.string(),
+    UPSTASH_REDIS_REST_TOKEN: z.string(),
   },
 
   /**
@@ -41,6 +42,7 @@ export const env = createEnv({
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
