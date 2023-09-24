fa21bb5d2c05f011b8c2fadb93cc2356dd1c7af8
Adam Szi <szi.adam@simonyi.bme.hu>
2023-09-24 18:44:38 +0200

    redirect from root to home

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,24 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
 
 // This example protects all routes including api/trpc routes
 // Please edit this to allow other routes to be public as needed.
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
 export default authMiddleware({
-  ignoredRoutes: ['/(api|trpc)(.*)'],
-  publicRoutes: ['/', '/testing/public'],
+  ignoredRoutes: ["/(api|trpc)(.*)"],
+  publicRoutes: ["/", "/testing/public"],
   debug: false,
+  afterAuth(auth, req) {
+    if (!auth.userId && !auth.isPublicRoute) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return redirectToSignIn({ returnBackUrl: req.url });
+    }
+
+    if (req.nextUrl.pathname === "/" && !!auth.userId) {
+      const home = new URL('/home', req.url)
+      return NextResponse.redirect(home);
+    }
+  },
 });
 
 export const config = {
